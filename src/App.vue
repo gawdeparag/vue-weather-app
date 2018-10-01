b65b5187f352c6d29109d1d19a773f3162329c7c
Parag Gawde <paraggawde.303@gmail.com>
2018-10-01 21:59:57 +0530

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Vue Weather Forecast</h2>
+    <weather/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import weather from './components/weather.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    weather
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+img{
+  height: 100px;
 }
 </style>
---
+++ src/components/weather.vue
@@ -0,0 +1,60 @@
+<template>
+    <div id="weather">
+        <form @submit.prevent="getWeather(inputLocation)">
+            <input type="text" v-model="inputLocation">
+            <button type="submit">Submit</button>
+        </form>
+        <br>
+        <div id="forecast">
+            <p v-if="temperature">
+                Country: {{country}} <br>
+                Temperature: {{temperature}}&#8451; <br/>
+                Pressure: {{pressure}} hpa <br>
+                Humidity: {{humidity}}% <br>
+                Wind Speed: {{windSpeed}} kn <br>
+            </p>
+            <p v-else-if="errorStatement">
+                {{errorStatement}}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "weather",
+  data: function() {
+    return {
+      inputLocation: "",
+      temperature: "",
+      pressure: "",
+      humidity: "",
+      windSpeed: "",
+      country: "",
+      errorStatement: ""
+    };
+  },
+  methods: {
+    getWeather: function(inputTitle) {
+      axios
+        .get(
+          "http://api.openweathermap.org/data/2.5/weather?q=" +
+            inputTitle +
+            "&appid=7ddbbac4414598a0582efd0c12096e75&units=metric"
+        )
+        .then(response => {
+          this.temperature = response.data.main.temp;
+          this.pressure = response.data.main.pressure;
+          this.humidity = response.data.main.humidity;
+          this.windSpeed = response.data.wind.speed;
+          this.country = response.data.sys.country;
+        })
+        .catch(error => {
+          this.errorStatement = "Is this a place on earth?? Couldn't find it though! :(";
+        });
+    }
+  }
+};
+</script>
+
